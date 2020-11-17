554a4620b40ecf86f1143e79a3fd1bdad61f11c5
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-17 11:04:03 +0100

    Can get gene names

--- create_gene_names.R
+++ create_gene_names.R
@@ -1,4 +1,33 @@
 # Get all human protein gene names
 gene_ids <- readr::read_csv("gene_ids.csv")
 
+t <- tibble::tibble(
+  gene_id = gene_ids,
+  gene_name = NA
+)
 
+# Do this per 100
+sample_size <- 100
+while (sum(is.na(t$gene_name)) > sample_size) {
+  indices <- sample(which(is.na(t$gene_name)), size = sample_size, replace = FALSE)
+  testthat::expect_equal(sample_size, length(indices))
+  testthat::expect_true(all( is.na(t$gene_name[indices])))
+  gene_ids <- as.character(unlist(t$gene_id[indices, ]))
+  testthat::expect_equal(length(indices), length(gene_ids))
+  gene_names <- ncbi::get_gene_names_from_human_gene_ids(gene_ids)
+  t$gene_name[indices] <- gene_names
+  testthat::expect_true(all(!is.na(t$gene_name[indices])))
+  Sys.sleep(0.51)
+}
+
+indices <- which(is.na(t$gene_name))
+testthat::expect_true(length(indices) < sample_size)
+testthat::expect_true(all(is.na(t$gene_name[indices])))
+gene_ids <- as.character(unlist(t$gene_id[indices, ]))
+testthat::expect_equal(length(indices), length(gene_ids))
+gene_names <- ncbi::get_gene_names_from_human_gene_ids(gene_ids)
+t$gene_name[indices] <- gene_names
+testthat::expect_true(all(!is.na(t$gene_name[indices])))
+Sys.sleep(0.51)
+
+readr::write_csv(x = t, file = "gene_names.csv")
