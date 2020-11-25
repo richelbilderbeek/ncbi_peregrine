2b201a9b3064b19925d55c779d7e5ae5259efa31
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-25 08:55:40 +0100

    Allow for proteins without SNPs

--- create_snp_variations_rds.R
+++ create_snp_variations_rds.R
@@ -47,18 +47,30 @@ for (j in seq(1, 10)) {
       next
     }
 
+
     # Per SNP, obtain the variation
     # Save to file after each SNP
     if (j <= length(tibbles)) {
       testthat::expect_true(tibble::is_tibble(tibbles[[j]]))
       next
     }
-    snp_id <- t_snp_ids$snp_id[j]
-    variations <- ncbi::get_snp_variations_in_protein_from_snp_id(snp_id)
-    tibbles[[j]] <- tibble::tibble(
-      snp_id = snp_id,
-      variation = variations,
-    )
+
+    if (nrow(t_snp_ids) != 0) {
+      # There are SNP IDs
+      snp_id <- t_snp_ids$snp_id[j]
+      variations <- ncbi::get_snp_variations_in_protein_from_snp_id(snp_id)
+      tibbles[[j]] <- tibble::tibble(
+        snp_id = snp_id,
+        variation = variations,
+      )
+    } else {
+      # Protein has no SNP IDs
+      tibbles[[j]] <- tibble::tibble(
+        snp_id = numeric(0),
+        variation = character(0),
+      )
+    }
+
     saveRDS(object = tibbles, file = rds_filename)
   }
 }
