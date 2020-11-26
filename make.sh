27ce5c29e7b9a13cc5f6feb718c4e57b8c50b94a
richelbilderbeek <richel@richelbilderbeek.nl>
2020-11-26 12:09:30 +0100

    Can make on Peregrine

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Bash script to call make with sbatch
+#
+# Usage:
+#
+#   sbatch make.sh
+#
+#   sbatch make.sh all
+#
+#SBATCH --time=240:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=make
+#SBATCH --output=make_%j.log
+module load R
+echo "make $@"
+make "$@"
