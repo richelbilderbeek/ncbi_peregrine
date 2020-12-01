5ac3d7b3e8f36e8bc4f50a4ef0b3ba6f34c70f1e
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-01 13:06:58 +0100

    Create all gene IDs

--- create_gene_ids.R
+++ create_gene_ids.R
@@ -1,10 +1,8 @@
 # Get all human protein gene IDs
 all_gene_ids <- ncbi::get_all_human_membrane_protein_gene_ids()
 
-n_gene_ids <- 3
-
 t <- tibble::tibble(
-  gene_id = head(all_gene_ids, n = n_gene_ids)
+  gene_id = all_gene_ids
 )
-t
+
 readr::write_csv(x = t, file = "gene_ids.csv")
