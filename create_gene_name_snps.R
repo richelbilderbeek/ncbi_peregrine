bb1fc9af0080f45a41d0087954608eec3128e524
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-02 08:12:33 +0100

    More expressive variable names

--- create_gene_name_snps.R
+++ create_gene_name_snps.R
@@ -8,29 +8,29 @@ t <- readr::read_csv(
 
 testthat::expect_true("gene_id" %in% names(t))
 testthat::expect_true("gene_name" %in% names(t))
-t$filename <- paste0(t$gene_id, "_snps.csv")
+t$snps_filename <- paste0(t$gene_id, "_snps.csv")
 
-if (!all(file.exists(t$filename))) {
-  if (file.exists("create_gene_name_snps_is_done.txt")) {
-    file.remove("create_gene_name_snps_is_done.txt")
-  }
-}
+# if (!all(file.exists(t$filename))) {
+#   if (file.exists("create_gene_name_snps_is_done.txt")) {
+#     file.remove("create_gene_name_snps_is_done.txt")
+#   }
+# }
 
 # Per gene name, save its SNPs
 for (i in seq_len(nrow(t))) {
-  filename <- t$filename[i]
-  if (file.exists(filename)) next
+  snps_filename <- t$snps_filename[i]
+  if (file.exists(snps_filename)) next
   gene_name <- t$gene_name[i]
   t_here <- tibble::tibble(
     snp_id = ncbi::get_snp_ids_from_gene_name(gene_name)
   )
   testthat::expect_true("snp_id" %in% names(t_here))
   if (nrow(t_here) > 0) {
-    readr::write_csv(x = t_here, file = filename)
+    readr::write_csv(x = t_here, file = snps_filename)
   } else {
     file.create(filename)
   }
-  testthat::expect_true(file.exists(filename))
+  testthat::expect_true(file.exists(snps_filename))
 }
 
 readr::write_lines(
