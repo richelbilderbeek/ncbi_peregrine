07356c6e180c019d7b932afdef9fc17e9e2cf2e0
richelbilderbeek <richel@richelbilderbeek.nl>
2020-12-15 07:53:39 +0100

    Add verbose

--- scripts/create_snp_variations_rds.R
+++ scripts/create_snp_variations_rds.R
@@ -1,11 +1,14 @@
 args <- commandArgs(trailingOnly = TRUE)
-testthat::expect_equal(1, length(args))
+testthat::expect_equal(2, length(args))
 n_snps <- as.numeric(args[1])
+verbose <- as.logical(args[2])
 message("n_snps: ", n_snps)
+message("verbose: ", verbose)
 
 ncbiperegrine::create_snp_variations_rds_files(
   snps_filenames = list.files(pattern = "_snps\\.csv"),
-  n_snps = n_snps
+  n_snps = n_snps,
+  verbose = verbose
 )
 
 readr::write_lines(
